621a1f1503ef4e7c615c4c6769ee45a88e3231cc
whdkfhr <whwoals18@naver.com>
2020-09-06 19:15:27 +0900

    등록/수정/조회 API

---
+++ src/main/java/me/arok/springboot/service/posts/PostsService.java
@@ -0,0 +1,35 @@
+package me.arok.springboot.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import me.arok.springboot.domain.posts.Posts;
+import me.arok.springboot.domain.posts.PostsRepository;
+import me.arok.springboot.web.dto.PostsResponseDto;
+import me.arok.springboot.web.dto.PostsSaveRequestDto;
+import me.arok.springboot.web.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+
+}
---
+++ src/main/java/me/arok/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package me.arok.springboot.web.dto;
+
+import lombok.Getter;
+import me.arok.springboot.domain.posts.Posts;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+
+}
