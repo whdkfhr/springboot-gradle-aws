c9cb294983eb21fbf84853c64ca58bb14464c73f
whdkfhr <whwoals18@naver.com>
2020-09-09 20:00:22 +0900

    기존 테스트에 시큐리티 적용

--- src/test/java/me/arok/springboot/web/HelloControllerTest.java
+++ src/test/java/me/arok/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package me.arok.springboot.web;
 
+import me.arok.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,14 +16,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+            excludeFilters = {
+            @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+            })
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void hello() throws Exception {
+    public void hello_return() throws Exception {
         String hello = "hello";
 
         mockMvc.perform(get("/hello"))
@@ -27,8 +35,9 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
-    public void helloDto() throws Exception {
+    public void helloDto_return() throws Exception {
         String name = "hello";
         int amount = 1000;
 
